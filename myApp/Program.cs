e088f5b371b9dbad6e0ff6047a77009c3edc7ed2
testcompat <appcompat1117@gmail.com>
2019-10-14 09:24:28 +0000

    commit1

---
+++ myApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstno = 10, secondno = 15;
+            int sum = firstno + secondno;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
